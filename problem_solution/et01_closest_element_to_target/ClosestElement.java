0c510465ce9d4fe0c03423e8ffa1937c4d701b33
RogerLuo-Code <roger_luo@outlook.com>
2020-05-04 07:13:38 -0400

    Update solution to ET03 with corner case handling

--- problem_solution/et01_closest_element_to_target/ClosestElement.java
+++ problem_solution/et01_closest_element_to_target/ClosestElement.java
@@ -1,37 +1,38 @@
 public class ClosestElement{
     private CloestElement(){}
 
-    public static int binarySearch(int[] a, int target){
-        int left = 0;
-        int right = a.length - 1; // -1 for zero-based indexing
-        if (right < 0)
+    public static int binarySearch(int[] array, int target){
+        if (array == null || array.length == 0)
             return -1;
+        
+        int left = 0;
+        int right = array.length - 1; // -1 for zero-based indexing
         int middle;
 
         while (left < right - 1){ // stop at left == right - 1, two elements left
             middle = left + (right - left)/2;
-            if (target < a[middle])
+            if (target < array[middle])
                 right = middle; // middle -1 may rule out the final result
-            else if (target > a[middle])
+            else if (target > array[middle])
                 left = middle; // middle +1 may rule out the final result
             else
                 return middle; 
         }
 
         // Two elements left and compare them
-        if (Math.abs(a[left] - target) <= Math.abs(a[right] - target))
+        if (Math.abs(array[left] - target) <= Math.abs(array[right] - target))
             return left;
         else
             return right;
     }
 
     public static void main(String[] args){
-        // int[] a = {-5, 1, 3, 9, 10, 25, 100};
-        int[] a =  {};
+        // int[] array = {-5, 1, 3, 9, 10, 25, 100};
+        int[] array =  {};
         int target = 101;
         int index; 
         
-        index = ClosestElement.binarySearch(a, target);
+        index = ClosestElement.binarySearch(array, target);
 
         System.out.println(index);
     }
