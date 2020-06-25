83bfb7505a556d2a6a54c9cdacbcc0be5c83ac8e
RogerLuo-Code <roger_luo@outlook.com>
2020-06-24 22:21:40 -0400

    Update LC33 python code

--- problem_solution/lc33_search_rotated_sorted_array/SearchRotatedArray.py
+++ problem_solution/lc33_search_rotated_sorted_array/SearchRotatedArray.py
@@ -3,8 +3,7 @@ class SearchRotatedArray:
         if not nums:
             return -1
         
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
         
         while (left <= right):
             mid = left + (right - left)//2
@@ -16,12 +15,12 @@ class SearchRotatedArray:
             # elif nums[mid] == nums[right]: # mid == right, 
             #     right = mid - 1
             elif nums[mid] >= nums[left]:
-                if target >= nums[left] and target < nums[mid]:
+                if nums[left] <= target and target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                if target > nums[mid] and target <= nums[right]:
+                if nums[mid] < target and target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
